b86f6df68926fb4936e93e65e87b681707ccec1e
shafran <shafran.andriy@gmail.com>
2026-04-21 19:11:55 +0300

    fix flake8 in main.py

--- app/main.py
+++ app/main.py
@@ -26,7 +26,8 @@ def get_weather() -> None:
 
     print(
         f"Current weather in {CITY}: "
-        f"{condition}, {temperature}C, feels like {feels_like}C, humidity {humidity}%"
+        f"{condition}, {temperature}C,
+        feels like {feels_like}C, humidity {humidity}%"
     )
 
 
